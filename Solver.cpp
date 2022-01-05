c04475ce4c85436f324e9788dfbbe1d6675a86fc
Maxim Pochinskiy <mvp280102@gmail.com>
2022-01-05 23:28:46 +0500

    Functions belong(), middle() and cover() added.

--- Solver.cpp
+++ Solver.cpp
@@ -1,2 +1,89 @@
 #include "Solver.h"
 
+/*
+ * Если точка принадлежит квадрату, возвращает true, иначе - false.
+ */
+bool Solver::belong(Point point, Square square, unsigned int side_length)
+{
+	return ((point.x > square.corner.x) && (point.x < square.corner.x + side_length)) &&
+			((point.y > square.corner.y) && (point.y < square.corner.y + side_length));
+}
+
+
+/*
+ * Если квадраты с заданными номерами пересекаются, возвращает
+ * координаты середины их пересечения, иначе {-1; -1}.
+ */
+Point* Solver::middle(unsigned int first, unsigned int second)
+{
+	Segment x1{}, x2{}, y1{}, y2{};
+	Point *req = nullptr;
+
+	x1.begin = data.squares[first].corner.x;
+	x1.end = x1.begin + data.side_length;
+
+	y1.begin = data.squares[first].corner.y;
+	y1.end = y1.begin + data.side_length;
+
+	x2.begin = data.squares[second].corner.x;
+	x2.end = x2.begin + data.side_length;
+
+	y2.begin = data.squares[second].corner.y;
+	y2.end = y2.begin + data.side_length;
+
+	if  (((x1.end > x2.begin) || (x2.end > x1.begin)) && ((y1.end > y2.begin) || (y2.end > y1.begin)))
+	{
+		req = new Point;
+
+		req->x = (x1.end + x2.begin) / 2.0;
+		req->y = (y1.end + y2.begin) / 2.0;
+	}
+
+	return req;
+}
+
+
+/*
+ * Возвращает вектор с координатами точек покрытия.
+ * В зависимости от переданного параметра улучшает точность или скорость работы.
+ */
+vector<Point> Solver::cover(bool accuracy_time)
+{
+	vector<Point> points;
+	Point *req;
+	bool intersected = false;
+
+	for (int i = 0; i < data.squares.size(); ++i)
+	{
+		if (intersected)
+		{
+			intersected = false;
+			continue;
+		}
+
+		for (int j = i + 1; j < data.squares.size(); ++j)
+		{
+			req = middle(i, j);
+
+			if (!data.squares[i].covered && !data.squares[j].covered && (req != nullptr))
+			{
+				data.squares[i].covered = data.squares[j].covered = true;
+				points.push_back(*req);
+
+				if (accuracy_time)
+					for (int k = 0; k < data.squares.size(); ++k)
+						if ((k != i) && (k != j) && belong(*req, data.squares[k], data.side_length))
+							data.squares[k].covered = true;
+
+				intersected = true;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < data.squares.size(); ++i)
+		if (!data.squares[i].covered)
+			points.push_back(*middle(i, i));
+
+	return points;
+}
--- Solver.h
+++ Solver.h
@@ -29,7 +29,6 @@ struct Segment
 // Набор данных для задачи.
 struct TaskData
 {
-	unsigned int squares_amount;        // Количество квадратов.
 	unsigned int side_length;           // Длина стороны одного квадрата.
 	vector<Square> squares;             // Вектор квадратов.
 };
@@ -42,9 +41,40 @@ class Solver
 
 	TaskData data;                      // Набор данных для задачи.
 
+	/*
+	 * Если точка принадлежит квадрату, возвращает true, иначе - false.
+	 *
+	 * Параметры:
+	 * Point point - точка
+	 * Square square - квадрат
+	 * unsigned int side_length - сторона квадрата
+	 */
+	static bool belong(Point point, Square square, unsigned int side_length);
+
+	/*
+	 * Если квадраты пересекаются, возвращает указатель на
+	 * координаты середины их пересечения, иначе - nullptr.
+	 *
+	 * Параметры:
+	 * unsigned int first - номер первого квадрата
+	 * unsigned int second - номер второго квадрата
+	 */
+	Point* middle(unsigned int first, unsigned int second);
+
 
 	public:
 
-	// Конструктор.
+	/*
+	 * Конструктор.
+	 */
 	explicit Solver(TaskData data) : data(move(data)) {}
+
+	/*
+	 * Возвращает вектор с координатами точек покрытия.
+	 * В зависимости от переданного параметра улучшает точность или скорость работы.
+	 *
+	 * Параметры:
+	 * bool time_accuracy - true улучшает точность, false - скорость работы
+	 */
+	vector<Point> cover(bool accuracy_time = true);
 };
