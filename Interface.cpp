b5e73c6c9a69801f3597c6512680212a26403c2a
Maxim Pochinskiy <mvp280102@gmail.com>
2022-01-10 13:16:50 +0500

    Data reading and solver initializing added.

--- Interface.cpp
+++ Interface.cpp
@@ -1,1 +1,31 @@
 #include "Interface.h"
+
+
+/*
+ * Считывает с клавиатуры данные для одной задачи и
+ * инициализирует ими экземпляр класса, решающего ее.
+ */
+void Interface::read_data()
+{
+	TaskData data{};
+	Point corner{};
+
+	unsigned int squares_amount;
+
+	cout << "Squares amount: ";
+	cin >> squares_amount;
+	cout << "Side length: ";
+	cin >> data.side_length;
+
+	data.squares.reserve(squares_amount);
+
+	cout << "Squares left corner coordinates:\n";
+
+	for (int i = 0; i < squares_amount; ++i)
+	{
+		cin >> corner.x >> corner.y;
+		data.squares.push_back(Square{false, Point{corner.x, corner.y}});
+	}
+
+	solver = new Solver(data);
+}
--- Interface.h
+++ Interface.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include "Solver.h"
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+// Класс, предоставляющий интерфейс для решения задачи по шагам.
 class Interface
 {
+	private:
 
+	Solver *solver = nullptr;               // Экземпляр класса, решающего задачу.
+
+
+	public:
+
+	/*
+	 * Считывает с клавиатуры данные для одной задачи и
+	 * инициализирует ими экземпляр класса, решающего ее.
+	 */
+	void read_data();
 };
